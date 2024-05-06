cdff33b9129717b03209c2c177a2ad77d71d46b6
arielbaltazar <115984983+arielbaltazar@users.noreply.github.com>
2024-05-06 15:13:24 +0100

    made main menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+
+    }
+}
